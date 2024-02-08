676244ab465e6c67183ece940bd4cae9afae52bc
VaneSanchez <nyagami@yahoo.com>
2024-02-08 19:09:16 -0300

    correcciónd e estilos de la app del clima

--- src/components/CreateTask.jsx
+++ src/components/CreateTask.jsx
@@ -1,56 +1,59 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import { MdAdd } from "react-icons/md";
 
 const CreateTask = ({ tasks, setTasks }) => {
+  const [task, setTask] = useState({
+    id: "",
+    name: "",
+    status: "todo",
+  });
 
-    const [task, setTask] = useState ({
-        id: "",
-        name:"",
-        status: "todo"
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (task.name.length < 3)
+      return toast.error("Las tareas deben tener mas de 3 letras...");
+
+    if (task.name.length > 100)
+      return toast.error("Las tareas deben tener menos de 100 letras...");
+
+    setTasks((prev) => {
+      const list = [...prev, task];
+
+      localStorage.setItem("tasks", JSON.stringify(list));
+
+      return list;
     });
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    toast.success("Nueva Tarea Pendiente");
 
-        if(task.name.length < 3) 
-            return toast.error("Las tareas deben tener mas de 3 letras...");
-        
-        if(task.name.length > 100) 
-            return toast.error("Las tareas deben tener menos de 100 letras...");
-
-
-        setTasks((prev) => {
-            const list = [...prev, task];
-
-            localStorage.setItem("tasks", JSON.stringify(list))
-
-            return list;
-
-        });
-
-        toast.success("Nueva Tarea Pendiente")
-
-        setTask({
-            id: "",
-            name:"",
-            status: "todo",
-        })
-    }
+    setTask({
+      id: "",
+      name: "",
+      status: "todo",
+    });
+  };
 
   return (
     <form onSubmit={handleSubmit} className="flex mx-auto">
-        <input 
-        type='text'
-        className='border-2 border-slate-400 bg-slate-100 rounded-l-md h-12 w-52 pl-3 '
-        placeholder="Tarea Nueva..."
-        value={task.name}
-        onChange={(e)=> setTask({...task, id: uuidv4(), name: e.target.value})}
+      <div class="pt-2 relative mx-auto text-gray-600">
+        <input
+          type="text"
+          className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-full text-sm focus:outline-none"
+          placeholder="Tarea Nueva..."
+          value={task.name}
+          onChange={(e) =>
+            setTask({ ...task, id: uuidv4(), name: e.target.value })
+          }
         />
-        <button className="bg-gray-600 rounded-r-md px-4 h-12 text-white"><MdAdd size={25}/></button>
+        <button className="absolute right-0 top-0 mt-5 mr-4">
+          <MdAdd className="text-gray-600 h-4 w-4 fill-current" size={30} />
+        </button>
+      </div>
     </form>
-  )
-}
+  );
+};
 
 export default CreateTask;
--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -5,16 +5,13 @@ import cloud_icon from "../assets/cloud.png";
 import drizzle_icon from "../assets/drizzle.png";
 import rain_icon from "../assets/rain.png";
 import snow_icon from "../assets/snow.png";
-import thunder_icon from "../assets/thunder.png";
 import wind_icon from "../assets/wind.png";
 import humidity_icon from "../assets/humidity.png";
 
 export const WeatherApp = () => {
   let api_key = "d526b3ca5fea41b8b93f133c4046b0c2";
 
-  const [wicon, setWicon] = useState(
-    localStorage.getItem("weatherIcon") || cloud_icon
-  );
+  const [wicon, sertWicon] = useState(cloud_icon);
 
   const search = async () => {
     const element = document.getElementsByClassName("cityInput");
@@ -32,76 +29,70 @@ export const WeatherApp = () => {
 
     humidity[0].innerHTML = data.main.humidity + " %";
     wind[0].innerHTML = Math.floor(data.wind.speed) + " km/h";
-    tempertature[0].innerHTML = Math.floor(data.main.temp) + " °c";
+    tempertature[0].innerHTML = Math.floor(data.main.temp) + "°";
     location[0].innerHTML = data.name;
 
     if (data.weather[0].icon === "01d" || data.weather[0].icon === "01n") {
-      setWicon(clear_icon);
+      sertWicon(clear_icon);
     } else if (
       data.weather[0].icon === "02d" ||
       data.weather[0].icon === "02n"
     ) {
-      setWicon(cloud_icon);
+      sertWicon(cloud_icon);
     } else if (
       data.weather[0].icon === "03d" ||
       data.weather[0].icon === "03n"
     ) {
-      setWicon(drizzle_icon);
+      sertWicon(drizzle_icon);
     } else if (
       data.weather[0].icon === "04d" ||
       data.weather[0].icon === "04n"
     ) {
-      setWicon(drizzle_icon);
+      sertWicon(drizzle_icon);
     } else if (
       data.weather[0].icon === "09d" ||
       data.weather[0].icon === "09n"
     ) {
-      setWicon(rain_icon);
+      sertWicon(rain_icon);
     } else if (
       data.weather[0].icon === "10d" ||
       data.weather[0].icon === "10n"
     ) {
-      setWicon(rain_icon);
-    } else if (
-      data.weather[0].icon === "11d" ||
-      data.weather[0].icon === "11n"
-    ) {
-      setWicon(thunder_icon);
+      sertWicon(rain_icon);
     } else if (
       data.weather[0].icon === "13d" ||
       data.weather[0].icon === "13n"
     ) {
-      setWicon(snow_icon);
+      sertWicon(snow_icon);
     } else {
-      setWicon(clear_icon);
+      sertWicon(clear_icon);
     }
-
-    setWicon(newIcon);
-    localStorage.setItem("weatherIcon", newIcon);
   };
 
   return (
     <div className="flex flex-col items-center">
-      <div className="flex items-center mt-4 gap-2">
+      <div className="pt-2 relative mx-auto text">
         <input
           type="text"
-          className="cityInput border-2 border-slate-400 bg-slate-100 rounded-full h-9 w-52 pl-3"
+          className="border-2 border-slate-300 bg-white h-10 px-5 pr-16 rounded-full text-sm focus:outline-none"
         />
         <button
-          className="bg-white rounded-full p-2"
+          className="absolute right-0 top-0 mt-5 mr-4"
           onClick={() => {
             search();
           }}
         >
-          <img src={search_icon} />
+          <img src={search_icon} className="text-gray-600 h-4 w-4 fill-current"/>
         </button>
       </div>
 
-      <div className="flex flex-col justify-center gap-2">
-        <div className="weather-icon flex flex-col items-center text-white font-bold">
+      <div className="flex flex-col justify-center">
+        <div className="wethear-location text-white font-bold text-2xl">
+          London
+        </div>
+        <div className="weather-icon flex items-center text-white font-bold">
           <img src={wicon} style={{ width: "120px", height: "120px" }} />
           <div className="wethear-temp text-6xl">24°</div>
-          <div className="wethear-location">London</div>
         </div>
 
         <div className="flex justify-center gap-4 pl-4">
